ff45cf43efbe3cd69b2dd71ea6ba3054a5877873
Don Bennett <dpb@donbennett.org>
2016-08-16 14:55:38 -0700

    script tweak.

--- scripts/SgRun.sh
+++ scripts/SgRun.sh
@@ -2,4 +2,4 @@
 
 . venv/bin/activate
 cd kafka-monitor
-./kafka_monitor.py feed '{"url": "https://seatgeek.com/concert-tickets/alternative", "appid":"sg", "crawlid":"sg1234", "maxdepth":3, "spiderid":"sg" }'
+./kafka_monitor.py feed '{"url": "https://seatgeek.com/concert-tickets/alternative", "appid":"sgapp", "crawlid":"sg1234", "maxdepth":3, "spiderid":"sg" }'
